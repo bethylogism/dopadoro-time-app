7f1c0a87a4ed57e77fb64c5cff8bf6d1a4cbe429
laceswingybethler <beth.swingler@gmail.com>
2021-03-19 14:27:40 +0000

    update colors, sizes

---
+++ src/utils/colors.ts
@@ -0,0 +1,5 @@
+export const colors = {
+  darkBlue: '#252250',
+  lightBlue: '#5E84E2',
+  white: '#feffef',
+};
--- src/utils/sizes.tsx
+++ src/utils/sizes.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
 
 export const fontSizes = {
-  sm: 8,
+  sm: 11,
   md: 16,
   lg: 24,
   xl: 32,
   xxl: 40,
   xxxl: 80,
+  sumo: 140,
 };
 
 export const spacing = {
-  sm: 8,
+  sm: 11,
   md: 16,
   lg: 24,
   xl: 32,
   xxl: 40,
   xxxl: 80,
+  sumo: 140,
 };
